15302cdac0478b012b051df30fdcfbfe9c123cba
Lei <jay6413682@gmail.com>
2015-01-20 14:16:23 -0800

    Re-construct the fixture based unit test module into multiple modules/files. Change the module name so that pytest could find them during discovery process. Fix some bugs introduced by this change.

---
+++ RallyAutomation/src/test/run/test/test_fixture_base.py
@@ -0,0 +1,42 @@
+from src.test.run.testObject import testObject
+import pytest
+import sys
+
+from pyrallei import Rally, rallyWorkset #By using custom package pyrallei as a workaround for the bug: bug: https://github.com/RallyTools/RallyRestToolkitForPython/issues/37
+import json
+
+import os
+
+#pytest fixture example
+@pytest.fixture(scope="module")
+def test_config_module(request):
+    try:
+        #global rally,data,to_obj
+        print ("setup_module      module:%s" % __name__)
+        parent_path=os.path.dirname(os.path.dirname(__file__))
+        options = ['--config='+parent_path+'/config.cfg'] #--config=config.cfg "extra.cfg"
+        server, user, password, apikey, workspace, project = rallyWorkset(options) #apikey can be obtained from https://rally1.rallydev.com/login/
+        #global rally
+        rally = Rally(server, user, password, workspace=workspace, project=project)
+        rally.enableLogging('rally.example.log', attrget=True, append=True)
+        #global data
+        # Read other configuration parameters from the extra.json
+        with open(parent_path+'/extra.json') as data_file:    
+            data = json.load(data_file)
+        #global to_obj
+        to_obj=testObject(rally,data)
+        
+        def fin():
+            print "teardown_module      module:%s" % __name__
+        request.addfinalizer(fin)
+        
+        return (to_obj,rally,data)
+    except Exception,details:
+        
+        print details
+        sys.exit(1)
+
+
+
+        
+        
