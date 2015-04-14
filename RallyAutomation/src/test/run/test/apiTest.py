6c5c543b3f77742df5eef4b392ca86dd81348c0c
Lei <jay6413682@gmail.com>
2015-04-13 18:37:56 -0700

    Reassign index to support new columns, add parameters to support extra fileds and etc.

--- RallyAutomation/src/test/run/test/apiTest.py
+++ RallyAutomation/src/test/run/test/apiTest.py
@@ -5,6 +5,7 @@ Created on Feb 3, 2015
 '''
 import requests
 import pycurl
+import json
 
 #s=requests.session()
 #login_data={'formPosted':'1', 'login_email':'me@example.com, 'password':'pw'}
@@ -22,21 +23,37 @@ response = s.get('http://10.40.60.170/login&nbsp;HTTP/1.1&nbsp;(application/x-ww
 pass
 '''
 
-url = "http://10.10.1.31/"
-
+url = "http://10.10.2.166/"
+ChassisIP="10.47.79.68"
 s = requests.session()
 #r = s.get(url,verify = False)
 
 
 payload = {
-'user[email]':'lei.jiang@spirent.com',
-'user[password]':'Jag6413682'
+"user[email]":"admin@spirent.com",
+"user[password]":"wrong"
 }
-r = s.post(url+'login',data=payload,verify = False)
 
-r2=s.get(url+'current_user')
+payload2={"user[email]":"standard@spirent.com","user[firstname]":"standard","user[lastname]":"standard","user[role]":"user","user[password]":"spirent"}
+r = s.post(url+'login',data=payload,verify = False)
+print r.content
+
+
+r2=s.get(url+'av_chassis/'+ChassisIP).content
+
+r2_dict=json.loads(r2)
+print r2_dict
+
+r4=s.get(url+'current_user').content
+print r4
+
+r5=s.post(url+'users', data=payload2, verify=False)
+print r5.content
+dict_user=json.loads(r5.content)
+r6=s.delete(url+'users/'+dict_user['id'])
+print r6.content
 
 r3= s.delete(url+'logout')
-
+print r3.content
 pass
 
