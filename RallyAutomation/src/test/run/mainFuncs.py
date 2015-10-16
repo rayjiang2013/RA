56e47616b935277a3665f5ef7f32ad22e31fcc80
Lei <jay6413682@gmail.com>
2015-10-15 18:56:50 -0700

    Add epydoc project file; add html folder generated by epydoc; rename loggers example to mainFuncs; add comments and stringdocs to mainFuncs

--- RallyAutomation/src/test/run/mainFuncs.py
+++ RallyAutomation/src/test/run/mainFuncs.py
@@ -16,51 +16,52 @@ from notification import Notification
 from reporting import Reporting
 #from sqlConnector import sqlConnector
 
-#The bugs https://github.com/RallyTools/RallyRestToolkitForPython/issues/37 and
-#https://github.com/RallyTools/RallyRestToolkitForPython/issues/40
-#should be fixed according comments from the po. But another bug
-#https://github.com/RallyTools/RallyRestToolkitForPython/issues/37
-#still exists; so use pyrallei instead
-#from pyral import Rally,rallyWorkset
-
 #By using custom package pyrallei as a workaround for the bug:
-#https://github.com/RallyTools/RallyRestToolkitForPython/issues/37;
-#have to switch to my personal package again for
 #https://github.com/jay6413682/RallyRestToolkitForPython/comit
 #/7dbec761a924ada0bdfe379b385a2b00e9875b21
+#Watch out for https://github.com/RallyTools/RallyRestToolkitForPython/pull/64
+#This pull request may be included in next release
+#We will be able to use the official pyral package again
 from pyrallei import Rally, rallyWorkset
+#from pyral import Rally,rallyWorkset
 
 #The main function
 def main():
     '''
-    This main() function is the start point of execution for the test automation framework.
-    It defines the flow of action the test automation framework executes.
+    @summary: This main() function is the start point of execution
+        for the test automation framework.
+        It defines the flow of action the test automation framework executes.
+    @return: no return
+    @status: under development
+    @raise details: log any exceptions
     '''
     if __name__ == '__main__':
         #Setup
         try:
             #logConfig("logging.json")
+            #:get logger from rallyLogger.setup function
             logger = setup(sys.argv[3])
             #logger = logging.getLogger(__name__)
+            # If you attach a handler to a logger and one or more of its ancestors,
+            # it may emit the same record multiple times.
+            # To prevent logger to pass to the handlers of higher level loggers
+            # set the propagate to false
             logger.propagate = False
+            #: Get Rally environment info from config.cfg
             options = [opt for opt in sys.argv[1:] if opt.startswith('--')]
-
-            #apikey can be obtained from https://rally1.rallydev.com/login/
+            #: apikey can be obtained from https://rally1.rallydev.com/login/
+            #: take all options and return useful info to setup connection to Rally
             server, user, password, apikey, workspace, project = rallyWorkset(options)
+            # write obtained info to debug log
             logger.debug("Rally project info is as below:\n %s", \
                          " ".join(['|%s|' % opt for opt in \
                         [server, user, password, apikey, workspace, project]]))
-
-            #include apikey=apikey to workaround
-            #http://stackoverflow.com/questions/30495164
-            #/getting-error-rallyrestapierror-422-not-authorized-to-perform-action-invalid
-            #and http://stackoverflow.com/questions/30492008
-            #/net-rally-restapi-error-not-authorized-to-perform-action-invalid-key-when-cr
+            #: setup session with rally
             rally = Rally(server, user, password, apikey=apikey, \
                           workspace=workspace, project=project)
 
-            rally.enableLogging('rally.example.log', attrget=True, append=True)
-            # Read other configuration parameters from the extra.json
+            #rally.enableLogging('rally.example.log', attrget=True, append=True)
+            # Read other test related configuration parameters from the extra.json
             with open(sys.argv[2]) as data_file:
                 data = json.load(data_file)
                 logger.debug("The extra.json configuration file contains \
@@ -77,20 +78,22 @@ def main():
             to_obj = testObject(rally, data)
             notification_obj = Notification(rally, data)
             report_obj = Reporting(rally, data)
+            # do sanity check first, if pass, run the tests
             if to_obj.sanityCheck(data['ts']['FormattedID'], *constants.CHECK_IP):
-                to_obj.getLastBuildInfoFromJenkins()
-                to_obj.updateBuildInfo()
-                to_obj.getLatestBuild()
-                ts_ut = to_obj.copyTS()
-                (verd, newdt) = to_obj.runTO(ts_ut)
-                test_results = to_obj.runTS(verd, newdt)
-                report = report_obj.genReport(test_results, constants.FROM_TR)
-                notification_obj.sendNotification(report)
+                to_obj.getLastBuildInfoFromJenkins() # Get latest build from jenkins
+                to_obj.updateBuildInfo() # create new build in Rally
+                to_obj.getLatestBuild() # Get latest build from rally
+                ts_ut = to_obj.copyTS() # copy test set to a new one in Rally
+                (verd, newdt) = to_obj.runTO(ts_ut) # run through test set
+                test_results = to_obj.runTS(verd, newdt) # update test results in Rally
+                report = report_obj.genReport(test_results, constants.FROM_TR) # generate report
+                notification_obj.sendNotification(report) # send email notification
         except Exception, details:
             #print details
-            logger.error('ERROR: %s \n', details, exc_info=True)
+            logger.error('ERROR: %s \n', details, exc_info=True) # log errors
             sys.exit(1)
 
+# run main function if run started from this module
 if __name__ == '__main__':
     main()
     
